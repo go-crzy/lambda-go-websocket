b880cd6f8aa4ead24eae6927f64fee9b1670a192
Gregory Guillou <10611760+gregoryguillou@users.noreply.github.com>
2021-04-17 10:30:15 +0200

    reorganize terraform resources (#4)

--- terraform/rx-apigw.tf
+++ terraform/rx-apigw.tf
@@ -1,16 +1,8 @@
 resource "aws_apigatewayv2_api" "websocket" {
-  name                       = "websocket"
+  name                       = var.rx
   protocol_type              = "WEBSOCKET"
-  description = "Websocket Gateway"
+  description = "Websocket Gateway for ${var.rx}"
   route_selection_expression = "$request.body.action"
-
-}
-
-resource "aws_apigatewayv2_api" "http" {
-  name                       = "http"
-  protocol_type              = "HTTP"
-  description = "HTTP Gateway"
-  route_selection_expression = "$request.method $request.path"
 
 }
 
@@ -24,18 +16,6 @@ resource "aws_apigatewayv2_integration" "websocket" {
   integration_method        = "POST"
   integration_uri           = aws_lambda_function.websocket.invoke_arn
   passthrough_behavior      = "WHEN_NO_MATCH"
-}
-
-
-resource "aws_apigatewayv2_integration" "http" {
-  api_id           = aws_apigatewayv2_api.http.id
-  integration_type = "AWS_PROXY"
-
-  connection_type           = "INTERNET"
-  description               = "Lambda example"
-  integration_method        = "POST"
-  integration_uri           = aws_lambda_function.http.invoke_arn
-  payload_format_version    = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "connect" {
@@ -56,20 +36,8 @@ resource "aws_apigatewayv2_route" "default" {
   target = "integrations/${aws_apigatewayv2_integration.websocket.id}"
 }
 
-resource "aws_apigatewayv2_route" "http" {
-  api_id           = aws_apigatewayv2_api.http.id
-  route_key        = "ANY /"
-  target = "integrations/${aws_apigatewayv2_integration.http.id}"
-}
-
-resource "aws_apigatewayv2_stage" "staging-websocket" {
+resource "aws_apigatewayv2_stage" "staging_websocket" {
   api_id      = aws_apigatewayv2_api.websocket.id
-  name        = "staging"
-  auto_deploy = true
-}
-
-resource "aws_apigatewayv2_stage" "staging-http" {
-  api_id      = aws_apigatewayv2_api.http.id
   name        = "staging"
   auto_deploy = true
 }
@@ -98,38 +66,26 @@ resource "aws_lambda_permission" "apigw_default" {
   source_arn    = "${aws_apigatewayv2_api.websocket.execution_arn}/*/$default"
 }
 
-resource "aws_lambda_permission" "apigw_http" {
-  statement_id  = "AllowHTTPFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.http.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http.execution_arn}/*/*/"
-}
-
-data "aws_iam_policy_document" "lambda_apigateway" {
+data "aws_iam_policy_document" "apigateway_rx" {
   statement {
     sid = "4"
     actions = [
       "execute-api:ManageConnections",
     ]
     resources = [
-      "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.websocket.id}/${aws_apigatewayv2_stage.staging-websocket.name}/POST/@connections/*",
+      "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.websocket.id}/${aws_apigatewayv2_stage.staging_websocket.name}/POST/@connections/*",
     ]
   }
 }
 
-resource "aws_iam_policy" "lambda_apigateway" {
-  name        = "lambda_apigateway"
+resource "aws_iam_policy" "apigateway_rx" {
+  name        = "apigateway_rx"
   path        = "/"
   description = "IAM policy interacting with API Gateway from a lambda"
 
-  policy = data.aws_iam_policy_document.lambda_apigateway.json
+  policy = data.aws_iam_policy_document.apigateway_rx.json
 }
 
 output wscat {
-	value = "wscat --connect ${aws_apigatewayv2_stage.staging-websocket.invoke_url}"
-}
-
-output curl {
-	value = "curl -XPOST -d'message' ${aws_apigatewayv2_stage.staging-http.invoke_url}/"
+	value = "wscat --connect ${aws_apigatewayv2_stage.staging_websocket.invoke_url}"
 }
